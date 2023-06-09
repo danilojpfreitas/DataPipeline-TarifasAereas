e7591ecee7e7cb2531af7221ff4a3a4696b2e882
Danilo Freitas <danilojpfreitas@gmail.com>
2023-06-09 18:08:18 +0000

    Notebook and Jupyter

--- setup.sh
+++ setup.sh
@@ -11,14 +11,20 @@ install() {
     #Instalando as bibliotecas para não ter erros nas Dags
     pip install pymysql xlrd openpyxl minio
     #Adicionando as variaveis no Airflow
-    # data_lake_server = 172.17.0.1:9000
+    # data_lake_server = 
     # data_lake_login = minioadmin
     # data_lake_password = minioadmin
-    # database_server = 172.17.0.3 ( Use o comando inspect para descobrir o ip do
-    # container: docker container inspect mysqlbd1 - localizar o atributo IPAddress)
-    # database_login = root
-    # database_password = stack
-    # database_name = employees
+
+    echo "Install Anaconda Docker Container..."
+    docker run -i -t -p 8888:8888 continuumio/anaconda3 /bin/bash -c "\
+    conda install jupyter -y --quiet && \
+    mkdir -p /opt/notebooks && \
+    jupyter notebook \
+    --notebook-dir=/opt/notebooks --ip='*' --port=8888 \
+    --no-browser --allow-root"
+    #Para acessar o Jupyter Notebook com os arquivos locais
+    jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root
+
 
     echo "Install Metabase..."
     mkdir metabase
